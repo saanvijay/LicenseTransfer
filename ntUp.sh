36c0a46a84fd11f98be118defe81356d5e84e956
saanvijay <mvjp.183@gmail.com>
2019-07-28 21:54:13 +0530

    network setup changes

--- ntUp.sh
+++ ntUp.sh
@@ -2,8 +2,53 @@
 export IMAGE_TAG=latest
 export PATH=${PWD}/../bin:${PWD}:$PATH
 
-#cryptogen generate --config=./crypto-config.yaml
+rm -rf crypto-config
+cryptogen generate --config=./crypto-config.yaml
 
+for Org  in Applee googlee microsoftt oraclee ibmm ;
+do
+cp $Org.yaml.tmp $Org.yaml 
+done
+
+ARCH=$(uname -s | grep Darwin)
+if [ "$ARCH" == "Darwin" ]; then
+  OPTS="-it"
+echo $OPTS
+else
+  OPTS="-i"
+fi
+
+CURRENT_DIR=$PWD
+cd crypto-config/peerOrganizations/Applee.com/ca/
+PRIV_KEY=$(ls *_sk)
+cd "$CURRENT_DIR"
+sed $OPTS "s/CA_APPLEE_KEY/${PRIV_KEY}/g" Applee.yaml
+
+CURRENT_DIR=$PWD
+cd crypto-config/peerOrganizations/oraclee.com/ca/
+PRIV_KEY=$(ls *_sk)
+cd "$CURRENT_DIR"
+sed $OPTS "s/CA_ORACLEE_KEY/${PRIV_KEY}/g" oraclee.yaml
+
+CURRENT_DIR=$PWD
+cd crypto-config/peerOrganizations/microsoftt.com/ca/
+PRIV_KEY=$(ls *_sk)
+cd "$CURRENT_DIR"
+sed $OPTS "s/CA_MICROSOFTT_KEY/${PRIV_KEY}/g" microsoftt.yaml
+
+CURRENT_DIR=$PWD
+cd crypto-config/peerOrganizations/googlee.com/ca/
+PRIV_KEY=$(ls *_sk)
+cd "$CURRENT_DIR"
+sed $OPTS "s/CA_GOOGLEE_KEY/${PRIV_KEY}/g" googlee.yaml
+
+CURRENT_DIR=$PWD
+cd crypto-config/peerOrganizations/ibmm.com/ca/
+PRIV_KEY=$(ls *_sk)
+cd "$CURRENT_DIR"
+sed $OPTS "s/CA_IBMM_KEY/${PRIV_KEY}/g" ibmm.yaml
+
+sleep 10 
 echo "=========================== Network down ============================="
 docker-compose -f Applee.yaml -f googlee.yaml -f microsoftt.yaml -f oraclee.yaml -f ibmm.yaml -f common.yaml down --volumes --remove-orphans
 echo "=========================== DONE ============================="
