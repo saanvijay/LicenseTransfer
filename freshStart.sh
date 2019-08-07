6eb3bae0de0629849a10579099fdb08da5d32fb4
saanvijay <mvjp.183@gmail.com>
2019-08-07 18:15:06 +0530

    enroll and register users

--- freshStart.sh
+++ freshStart.sh
@@ -1,28 +1,52 @@
 #!/bin/bash
 
-cd license-network
-./ntUp.sh restart
-cd -
+#cd license-network
+#./ntUp.sh 
+#cd -
 
 cd application
-rm -rf hfc-key-store hfc-key-store2
+rm -rf key-store-Applee key-store-ibmm key-store-oraclee key-store-googlee key-store-microsoftt
 
 echo
-echo -e "\e[1;32m ========= Enroll Admin for Vodafone =========== \e[0m"
+echo -e "\e[1;32m ========= Enroll Admin for Applee =========== \e[0m"
 echo
-node enrollAdmin.js
+node enrollAdminApplee.js
 echo
-echo -e "\e[1;32m ========= Enroll Admin for Allinq =========== \e[0m"
+echo -e "\e[1;32m ========= Enroll Admin for ibmm =========== \e[0m"
 echo
-node enrollAdmin2.js
+node enrollAdminIbmm.js
 echo
-echo -e "\e[1;32m ========= Register User1 for Vodafone =========== \e[0m"
+echo -e "\e[1;32m ========= Enroll Admin for oraclee =========== \e[0m"
 echo
-node registerUser.js
+node enrollAdminOraclee.js
 echo
-echo -e "\e[1;32m ========= Register User2 for Allinq =========== \e[0m"
+echo -e "\e[1;32m ========= Enroll Admin for microsoftt =========== \e[0m"
 echo
-node registerUser2.js
+node enrollAdminMicrosoftt.js
+echo
+echo -e "\e[1;32m ========= Enroll Admin for googlee =========== \e[0m"
+echo
+node enrollAdminGooglee.js
+echo
+echo -e "\e[1;32m ========= Register User1 for Applee =========== \e[0m"
+echo
+node registerUserApplee.js
+echo
+echo -e "\e[1;32m ========= Register User1 for Oraclee =========== \e[0m"
+echo
+node registerUserOraclee.js
+echo
+echo -e "\e[1;32m ========= Register User1 for Microsoftt =========== \e[0m"
+echo
+node registerUserMicrosoftt.js
+echo
+echo -e "\e[1;32m ========= Register User1 for Ibmm =========== \e[0m"
+echo
+node registerUserIbmm.js
+echo
+echo -e "\e[1;32m ========= Register User1 for Googlee =========== \e[0m"
+echo
+node registerUserGooglee.js
 echo
 echo -e "\e[1;32m ========= Start Application Server =========== \e[0m"
 echo
